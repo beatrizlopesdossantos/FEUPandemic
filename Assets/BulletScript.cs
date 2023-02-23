be612c57a1a4624d20372b1443837defe18efb22
Bibs <mbrlopes.santos@gmail.com>
2023-02-23 02:38:40 +0000

    Shooting and enemy killed done

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Camera mainCam;
+    private Rigidbody2D rb;
+    public float speed;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectsWithTag("MainCamera")[0].GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * speed;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 10) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Virus")) {
+            Destroy(gameObject);
+        }
+    }
+}
