37f3648dc71bf48c31aaaa85d7d5b38e67c0fcd6
Bruno Rosendo <brunomrosendo@hotmail.com>
2023-02-23 18:40:04 +0000

    Added attack animation to virus

--- Assets/Scripts/PlayerFollower.cs
+++ Assets/Scripts/PlayerFollower.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class PlayerFollower : MonoBehaviour
 {
     private GameObject objectToFollow;
+    private Animator anim;
     [SerializeField] private float velocity = 1f;
 
     private void Start()
     {
         objectToFollow = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         transform.position =
             Vector2.MoveTowards(transform.position, objectToFollow.transform.position, Time.deltaTime * velocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            anim.SetTrigger("attack");
+        }
     }
 }
