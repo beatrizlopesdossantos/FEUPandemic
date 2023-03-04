d238f5d309a95250bb2440216f6c229d05ddd034
Bruno Rosendo <brunomrosendo@hotmail.com>
2023-03-04 21:59:40 +0000

    Created frenzy effect and added syringe sprite

--- Assets/Scripts/Player/Attack/Shooting.cs
+++ Assets/Scripts/Player/Attack/Shooting.cs
@@ -7,12 +7,33 @@ public class Shooting : MonoBehaviour
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform firePoint;
     private bool canFire = true;
+    private int frenzyFireCount = 0; // counts the number of frenzy effects applied
     private float timer = 0;
     public float timeBetweenShots = 0.5f;
+    public float timeBetweenShotsFrenzy = 0.1f;
 
     void Update()
     {
-        if (!canFire) {
+        if (frenzyFireCount == 0) {
+            NormalFire();
+        } else {
+            AutomaticFire();
+        }
+    }
+
+    public void ActivateFrenzyFire() {
+        this.frenzyFireCount++;
+        this.timer = timeBetweenShotsFrenzy;
+    }
+
+    public void DeactivateFrenzyFire() {
+        this.frenzyFireCount--;
+        this.timer = 0;
+    }
+
+    private void NormalFire() {
+        if (!canFire)
+        {
             timer += Time.deltaTime;
             if (timer > timeBetweenShots)
             {
@@ -21,10 +42,20 @@ public class Shooting : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButtonDown(0) && canFire) {
+        if (Input.GetMouseButtonDown(0) && canFire)
+        {
             canFire = false;
-            Instantiate(bullet, firePoint.transform.position, Quaternion.identity);
+            Instantiate(bullet, firePoint.transform.position, bullet.transform.rotation);
             timer = 0;
+        }
+    }
+
+    private void AutomaticFire() {
+        if (timer <= timeBetweenShotsFrenzy) {
+            timer += Time.deltaTime;
+        } else {
+            timer = 0;
+            Instantiate(bullet, firePoint.transform.position, bullet.transform.rotation);
         }
     }
 }
