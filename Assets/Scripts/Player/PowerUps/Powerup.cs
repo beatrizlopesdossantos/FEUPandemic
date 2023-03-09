63e90db139d43217614bd78dbe768f5d9f45e8fa
Bibs <mbrlopes.santos@gmail.com>
2023-03-09 15:39:15 +0000

    Collisions and powerup Timer

--- Assets/Scripts/Player/PowerUps/Powerup.cs
+++ Assets/Scripts/Player/PowerUps/Powerup.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Powerup : MonoBehaviour
 {
     [SerializeField] private PowerupEffect effect;
     [SerializeField] private AudioSource audioSource;
+    private Image powerUpImage;
+    private float timer = 0;
+    private bool start = false;
+
+    private void Start() {
+        powerUpImage = GameObject.FindGameObjectWithTag("PowerUpTimer").GetComponent<Image>();
+        powerUpImage.fillAmount = 0;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -28,12 +37,27 @@ public class Powerup : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<Collider2D>().enabled = false;
+        powerUpImage.fillAmount = 1;
 
         TemporaryEffect effect = this.effect as TemporaryEffect;
+        start = true;
         effect.Apply(target);
         yield return new WaitForSeconds(effect.duration);
         effect.Remove(target);
 
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (this.effect is TemporaryEffect && this.start) {
+            timer += Time.deltaTime;
+            TemporaryEffect effect = this.effect as TemporaryEffect;
+            if (timer > effect.duration)
+            {
+                timer = effect.duration;
+            }
+            powerUpImage.fillAmount = 1 - timer / effect.duration;
+        }
     }
 }
