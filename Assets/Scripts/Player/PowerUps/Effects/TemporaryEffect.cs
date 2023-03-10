58f1f081ec53bc67072edcdb183e4a802b01c1f6
Bibs <mbrlopes.santos@gmail.com>
2023-03-10 01:16:21 +0000

    Added powerup container

--- Assets/Scripts/Player/PowerUps/Effects/TemporaryEffect.cs
+++ Assets/Scripts/Player/PowerUps/Effects/TemporaryEffect.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class TemporaryEffect : PowerupEffect
 {
     public float duration = 5f;
+    [SerializeField] public Sprite powerUpImage;
 
     public abstract void Remove(GameObject target);
 }
--- Assets/Scripts/Player/PowerUps/Powerup.cs
+++ Assets/Scripts/Player/PowerUps/Powerup.cs
@@ -7,15 +7,10 @@ public class Powerup : MonoBehaviour
 {
     [SerializeField] private PowerupEffect effect;
     [SerializeField] private AudioSource audioSource;
-    private Image powerUpImage;
+    private Image powerUpFill = null;
+    private Image currentImage = null;
     private float timer = 0;
     private bool start = false;
-
-    private void Start() {
-        powerUpImage = GameObject.FindGameObjectWithTag("PowerUpTimer").GetComponent<Image>();
-        powerUpImage.fillAmount = 0;
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -37,10 +32,44 @@ public class Powerup : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<Collider2D>().enabled = false;
-        powerUpImage.fillAmount = 1;
 
         TemporaryEffect effect = this.effect as TemporaryEffect;
         start = true;
+        int i = 0;
+        GameObject[] powerUpContainers = GameObject.FindGameObjectsWithTag("Container");
+        foreach (GameObject powerUpContainer in powerUpContainers) {
+            currentImage = powerUpContainer.GetComponent<Image>();
+            if (currentImage.sprite == null) {
+                Color imageColor = currentImage.color;
+                imageColor.a = 1f;
+                currentImage.color = imageColor;
+                currentImage.sprite = (this.effect as TemporaryEffect).powerUpImage;
+                break;
+            }
+            currentImage = null;
+            i++;
+        }
+        GameObject[] powerUpTimers = GameObject.FindGameObjectsWithTag("PowerUpTimer");
+        if (i < 3) powerUpFill = powerUpTimers[i].GetComponent<Image>();
+        float maxFill = 0;
+
+        for (int j = 0; j < powerUpTimers.Length; j++) {
+            if (currentImage == null && i == 3) {
+                if (powerUpTimers[j].GetComponent<Image>().fillAmount > maxFill) {
+                    maxFill = powerUpTimers[j].GetComponent<Image>().fillAmount;
+                    powerUpFill = powerUpTimers[j].GetComponent<Image>();
+                    powerUpFill.fillAmount = 1;
+                    currentImage = powerUpContainers[j].GetComponent<Image>();
+                }
+            }
+        }
+        if (i == 3) {
+            Color imageColor = currentImage.color;
+            imageColor.a = 1f;
+            currentImage.color = imageColor;
+            currentImage.sprite = (this.effect as TemporaryEffect).powerUpImage;
+        }
+
         effect.Apply(target);
         yield return new WaitForSeconds(effect.duration);
         effect.Remove(target);
@@ -55,9 +84,17 @@ public class Powerup : MonoBehaviour
             TemporaryEffect effect = this.effect as TemporaryEffect;
             if (timer > effect.duration)
             {
+                if (currentImage == null) {
+                    return;
+                }
+                Color imageColor = currentImage.color;
+                imageColor.a = 0f;
+                currentImage.color = imageColor;
+                currentImage.sprite = null;
                 timer = effect.duration;
             }
-            powerUpImage.fillAmount = 1 - timer / effect.duration;
+
+            if (powerUpFill != null) powerUpFill.fillAmount = 1 - timer / effect.duration;
         }
     }
 }
